ec5f30d546eab471f40d05f3ea746fef0bebf8e7
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-04-07 10:47:10 +0200

    Remove task function added

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,10 +3,35 @@ var app = new Vue(
     {
         el: '#root',
         data:{
-
+            tasks: [
+                {               
+                text: 'Fare la spesa',
+                done: false
+                },
+                {               
+                text: 'Portare a spasso il cane',
+                done: false
+                },
+                {               
+                text: 'Preparare una torta',
+                done: false
+                },
+                {               
+                text: 'Chiamare Anna',
+                done: false
+                },
+                {               
+                text: 'Comprare regalo',
+                done: false
+                },
+            ]
+            
         },
         methods: {
-            
+            //Funzione per rimuovere il task al click della x
+            removeTask: function(taskIndex){
+                this.tasks.splice(taskIndex, 1);
+            }
         }
     }
 );
